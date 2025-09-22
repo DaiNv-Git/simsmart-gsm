d8fe94bd1f63b20c4694a9daf005dc84e997ede3
DaiNv-Git <nguyenvandaicd@gmail.com>
2025-09-22 23:05:36 +0700

    first commit

---
+++ src/main/java/app/simsmartgsm/SimsmartGsmApplication.java
@@ -0,0 +1,13 @@
+package app.simsmartgsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsmartGsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimsmartGsmApplication.class, args);
+	}
+
+}
